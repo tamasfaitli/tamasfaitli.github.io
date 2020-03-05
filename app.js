38e7d5e3fb6a3a49baebc1f23fb2f25a9663cfec
Tamas Faitli <faitlit@gmail.com>
2020-03-06 00:17:46 +0200

    fixing about section

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-function change_text(text) {
-
-}
-
 var app = new Vue({
     el: '#app',
     data: {
         rotating_welcome_text_counter: 0,
-        rotating_welcome_text_list: ["scroll down!", "check my work!", "contact me!"],
-        rotating_welcome_text: 'scroll down!'
+        rotating_welcome_text1_list: ["scroll", "check", "contact"],
+        rotating_welcome_text2_list: [" down!", " my work!", " me!"],
+        rotating_welcome_text1: "scroll",
+        rotating_welcome_text2: " down!",
     },
     methods: {
         changeText: function() {
             this.rotating_welcome_text_counter += 1;
-            this.rotating_welcome_text = this.rotating_welcome_text_list[this.rotating_welcome_text_counter % this.rotating_welcome_text_list.length];
+            this.rotating_welcome_text1 = this.rotating_welcome_text1_list[this.rotating_welcome_text_counter % this.rotating_welcome_text1_list.length];
+            this.rotating_welcome_text2 = this.rotating_welcome_text2_list[this.rotating_welcome_text_counter % this.rotating_welcome_text2_list.length];
         }
     }
 })
@@ -24,18 +23,41 @@ function rotating_welcome_text_timer() {
     var d = new Date();
     app.changeText();
 
-    var rotating_welcome_text_baffle = baffle(".baffle-anim");
 
-    rotating_welcome_text_baffle.text(currentText => app.rotating_welcome_text);
+    var rotating_welcome_text_baffle1 = baffle("#baffle1");
+    var rotating_welcome_text_baffle2 = baffle("#baffle2");
 
-    rotating_welcome_text_baffle.set({
-        characters: '▒█/▓ ░█<░ ▒░▓ ▒█/▒ ░████ >>▒ █<▒<░ ▒>░█▒ ███',
-        speed: 60
+    var baffles = [rotating_welcome_text_baffle1, rotating_welcome_text_baffle2];
+    app_texts = [app.rotating_welcome_text1, app.rotating_welcome_text2];
+
+    baffles.forEach(baffle => {
+        baffle.text(currentText => app_texts[baffles.indexOf(baffle)])
+
+        baffle.set({
+            characters: '▒█/▓ ░█<░ ▒░▓ ▒█/▒ ░████ >>▒ █<▒<░ ▒>░█▒ ███',
+            speed: 60
+        })
+
+        baffle.start();
+        baffle.reveal(1100);
     });
+    /*
+        rotating_welcome_text_baffle1.text(currentText => app.rotating_welcome_text1);
+        rotating_welcome_text_baffle2.text(currentText => app.rotating_welcome_text2);
 
-    rotating_welcome_text_baffle.start();
-    rotating_welcome_text_baffle.reveal(1100);
+        rotating_welcome_text_baffle1.set({
+            characters: '▒█/▓ ░█<░ ▒░▓ ▒█/▒ ░████ >>▒ █<▒<░ ▒>░█▒ ███',
+            speed: 60
+        });
 
+        otating_welcome_text_baffle2.set({
+            characters: '▒█/▓ ░█<░ ▒░▓ ▒█/▒ ░████ >>▒ █<▒<░ ▒>░█▒ ███',
+            speed: 60
+        });
+
+        rotating_welcome_text_baffle.start();
+        rotating_welcome_text_baffle.reveal(1100);
+    */
 }
 
 
